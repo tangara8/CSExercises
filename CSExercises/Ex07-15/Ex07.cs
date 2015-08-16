797205011cde2cfd4cc142ad358e0504d35b1996
tangara8 <E0005415@u.nus.edu>
2015-08-16 18:31:43 +0800

    Update Ex07.cs
    Calculate TotalIncome

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,14 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double Salary;
+            double TotalIncome;
+            Console.WriteLine("Enter your salary:");
+            Salary = Convert.ToDouble((Console.ReadLine()));
+            double HousingAllowance = 0.1 * Salary;
+            double TransportAllowance = 0.03 * Salary;
+            TotalIncome = Salary + HousingAllowance + TransportAllowance;            
+            Console.WriteLine("Your TotalIncome is : " + TotalIncome);
         }
     }
 }
