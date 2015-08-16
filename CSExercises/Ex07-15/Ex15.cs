89ac3cde91d0d1b2b69d11142594ee237b84b1bb
tangara8 <E0005415@u.nus.edu>
2015-08-17 00:19:02 +0800

    Update Ex15.cs
    Determine ArmStrong number

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -4,7 +4,21 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            int number, remainder, sum = 0;
+            Console.Write("Enter the no: ");
+            number = int.Parse(Console.ReadLine());
+            for(int i = number; i > 0, i = i/10)
+            {
+                remainder = i%10;
+                sum = sum + remainer*remainder*remainder;
+            }
+            if (sum == number)
+            {
+                Console.Write("Enter no is an ArmStrong no");
+            }
+            else
+                 Console.Write("Enter no is not an ArmStrong no");
+                 Console.ReadLine();    
         }
     }
 }
