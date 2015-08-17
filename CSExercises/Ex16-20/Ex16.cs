efcaeecb285b50795a7e9c0ebdf38369bae55cdd
tangara8 <E0005415@u.nus.edu>
2015-08-17 11:10:06 +0800

    Update Ex16.cs
    Good Morning, male or female follows by name

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -4,7 +4,18 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            String name;
+            Console.WriteLine("Please enter your name:");
+            String gender;
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your gender(M/F):");
+            gender = Console.ReadLine();
+            if (gender == "M" || gender == "m")
+                Console.WriteLine("Good Morning Mr " + name);
+                else 
+                Console.WriteLine("Good Morning Ms " + name);
+            Console.ReadLine();    
         }
     }
-}
+        }
+        
