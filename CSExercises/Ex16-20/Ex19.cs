b6ac7482dcadee47326f729298391919e4585c96
tangara8 <E0005415@u.nus.edu>
2015-08-17 16:29:40 +0800

    Update Ex19.cs
    {0: .00} - to put in 0: before the  .00; no digit is added to .00 to accomodate big amounts.

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,10 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter the distance travelled in km :");
+
+            double distanceTravelledInKilometre = Convert.ToDouble(Console.ReadLine());
+            double distanceTravelledInMetre = distanceTravelledInKilometre * 100;
+            double minCharges = 2.40;
+            double nextCharge = 85 * 0.04;
+            double distanceTravelledAfter = (distanceTravelledInMetre - 90) * 0.05; // for distance travlled more than 9km
+            double totalCost;
+            if (distanceTravelledInKilometre >= 9)
+            {
+
+                totalCost = minCharges + nextCharge + distanceTravelledAfter;
+                Console.WriteLine("${0:.00}", totalCost);
+                Console.ReadLine();
+            }
+            else if (distanceTravelledInKilometre < 9)
+            {
+                totalCost = minCharges + nextCharge;
+                Console.WriteLine("${0:.00}", totalCost);
+                Console.ReadLine();
+            }
         }
     }
 }
+       
