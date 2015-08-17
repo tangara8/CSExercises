b4b9558bed681d389f8c2a0ae1eb5a29a65d3739
tangara8 <E0005415@u.nus.edu>
2015-08-17 15:18:10 +0800

    Update Ex18-ver2
    Need to put in Console.ReadLine() after each WriteLine(); or the command prompt console viewer will disappeared.

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -14,31 +14,36 @@ namespace CSExercises
                 Console.WriteLine("You score {0} which is grade {1}", marks, grade);
             }
 
-            if (marks >= 60 && marks <= 79)
+            if ((marks >= 60) && (marks <= 79))
             {
                 grade = "B";
-                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
+                Console.WriteLine("You score {0} + marks which is grade{1}", marks, grade);
+                Console.ReadLine();
             }
 
-            if (marks >= 40 && marks <= 59)
+            if ((marks >= 40) && (marks <= 59))
             {
                 grade = "C";
-                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
+                Console.WriteLine("You score " + marks + "which is" + "grade " + grade);
+                Console.ReadLine();
             }
-            if (marks <= 40)
+            if ((marks <= 40) && (marks >= 0))
             {
 
-                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
-                if (marks < 0 || marks > 100)
-                {
-                    Console.WriteLine("You have entered the marks wrongly.  Please enter again:");
-                }
-
-
+                Console.WriteLine("You score " + marks + "which is" + "grade " + grade);
+                Console.ReadLine();
+            }
+            if ((marks < 0) || (marks > 100))
+            {
+                Console.WriteLine("You have entered the marks wrongly.  Please enter again:");
+                Console.ReadLine();
 
             }
         }
     }
 }
 
+
+
+            
         
