152874df09e2d28adb9089bb2606592769fa15c5
tangara8 <E0005415@u.nus.edu>
2015-08-17 12:22:38 +0800

    Update Ex18.cs
    Can't use switch for this case as it doesn't allow range

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -4,7 +4,41 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter your marks:");
+            int marks = Convert.ToInt32(Console.ReadLine());
+            String grade = "";
+
+            if (marks >= 80)
+            {
+                grade = "A";
+                Console.WriteLine("You score {0} which is grade {1}", marks, grade);
+            }
+
+            if (marks >= 60 && marks <= 79)
+            {
+                grade = "B";
+                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
+            }
+
+            if (marks >= 40 && marks <= 59)
+            {
+                grade = "C";
+                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
+            }
+            if (marks <= 40)
+            {
+
+                Console.WriteLine("You score " + marks + "which is" + "grade" + grade);
+                if (marks < 0 || marks > 100)
+                {
+                    Console.WriteLine("You have entered the marks wrongly.  Please enter again:");
+                }
+
+
+
+            }
         }
     }
 }
+
+        
