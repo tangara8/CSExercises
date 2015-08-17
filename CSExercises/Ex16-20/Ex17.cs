03074960c0b7d1b438ae1335a5d590fb5b372b7f
tangara8 <E0005415@u.nus.edu>
2015-08-17 11:36:37 +0800

    Update Ex17.cs
    If > 40 is Uncle and Aunty

--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -1,10 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace CSExercises
 {
     public class Ex17
     {
         public static void Main(string[] args)
         {
-            
+            public static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("Please enter your name:");
+            String gender;
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your gender(M/F):");
+            gender = Console.ReadLine();
+            //int age;
+            Console.WriteLine("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());            
+            if ((gender == "M" || gender == "m") && (age >= 40))
+                Console.WriteLine("Good Morning Uncle " + name);
+            else
+                Console.WriteLine("Good Morning Mr " + name);
+            if ((gender == "F" || gender == "f") && (age >= 40))
+                Console.WriteLine("Good Morning Aunty " + name);
+            else
+                Console.WriteLine("Good Morning Ms " + name);
+            Console.ReadLine();    
         }
     }
 }
+        
