128f10a1e39da2b1df7c45c97d453f9935b05ec2
tangara8 <E0005415@u.nus.edu>
2015-08-18 14:16:45 +0800

    Update Ex10-ver2.cs
    Put the code right.

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,19 +10,15 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            int X1;
-            int Y1;
-            int X2;
-            int Y2;
-
+            
             Console.Write("Enter x1 : ");
             Console.Write("Enter y1 : ");
             Console.Write("Enter x2 : ");
             Console.Write("Enter y2 : ");
-            x1 = Convert.ToInt32(Console.ReadLine());
-            y1 = Convert.ToInt32(Console.ReadLine());
-            x2 = Convert.ToInt32(Console.ReadLine());
-            y2 = Convert.ToInt32(Console.ReadLine());
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Distance is " + (Math.Sqrt(x2 - x1) * (x2 - x1) + Math.Sqrt(y2 - y1) * (y2 - y1)));
             Console.ReadLine();
         }
