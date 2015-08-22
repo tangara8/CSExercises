155c1495a3158ab8986eb15125e79a0ba97b9811
tangara8 <E0005415@u.nus.edu>
2015-08-22 18:20:26 +0800

    Update.Ex33.cs

--- CSExercises/Ex32-34/Ex33.cs
+++ CSExercises/Ex32-34/Ex33.cs
@@ -13,30 +13,23 @@ using System.Threading.Tasks;
         public static void Main(string[] args)
         {
         int[] numbers = new int[20] { 86, 35, 41, 49, 89, 51, 11, 71, 50, 75, 25, 58, 40, 14, 44, 3, 63, 81, 93, 79 };
-            int i, j, k = 0, x = 0, temp;
-            for (i = 0; i < numbers.Length - 1; i++)
+        int temp = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                x = numbers[i + 1];
-                for (j = i + 1; j < numbers.Length; j++)
+                for (int j = i + 1; j < numbers.Length; j++)
                 {
-                    if (x < numbers[j])
+                    if (numbers[i] < numbers[j])
                     {
-                        x = numbers[j];
-                        k = j;
+                        temp = numbers[j];
+                        numbers[j] = numbers[i];
+                        numbers[i] = temp;
                     }
                 }
-                if (numbers[i] < x)
-                {
-                    temp = numbers[i];
-                    numbers[i] = numbers[k];
-                    numbers[k] = temp;
+                Console.WriteLine("{0}", numbers[i]);
+            }
+                        Console.ReadLine();
+                    }
                 }
             }
-            Console.WriteLine("Descending order:");
-            for (i = 0; i < numbers.Length; i++)
-                Console.WriteLine(numbers[i]);
-            Console.ReadLine();
-        }    
-        }
-    }
-
+   
+    
