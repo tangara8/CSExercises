6d0899f02387da868c8c466a023bac476b960fa1
tangara8 <E0005415@u.nus.edu>
2015-08-25 16:54:39 +0800

    Update Ex34

--- CSExercises/Ex32-34/Ex34.cs
+++ CSExercises/Ex32-34/Ex34.cs
@@ -12,48 +12,69 @@ using System.Threading.Tasks;
     {
         public static void Main(string[] args)
         {
-            int[,] Marks = new int[,] { { 56, 84, 68, 29 }, { 94, 73, 31, 89 }, { 41, 63, 36, 90 }, { 99, 9, 18, 17 }, 
-                    { 62, 3, 65, 75 }, { 40, 96, 53, 23 }, { 81, 15, 27, 30 }, { 21, 70, 100, 22 },
-                    { 88, 50, 13, 12 }, { 48, 54, 52, 78 }, { 64, 71, 67, 25 }, { 16, 93, 46, 72 } };
-            int[] m = new int[4];
-            int i = 0, j, sum;
-            //total marks
-            for (i = 0; i < 12; i++)
+             int[,] Marks = new int[12, 4]
+            {    
+                {56, 84, 68, 29},
+                {94, 73, 31, 89},
+                {41, 63, 36, 90},
+                {99, 9, 18, 17},
+                {62, 3, 65, 75},
+                {40, 96, 53, 23},
+                {81, 15, 27, 30},
+                {21, 70, 100, 22},
+                {88, 50, 13, 12},
+                {48, 54, 52, 78},
+                {64, 71, 67, 25},
+                {16, 93, 46, 72},
+                };
+            int sum;
+
+            for (int i = 0; i < Marks.GetLength(0); i++)
             {
-                sum = 0;
-                for (j = 0; j < 4; j++)
+
+                sum = 0; // putting sum here so that the Marks for row 1 will not be add on to row 2 and so on
+                for (int j = 0; j < Marks.GetLength(1); j++)
                 {
                     sum = sum + Marks[i, j];
-                }
-                Console.WriteLine("Total Marks of Student {0} is: {1}", i, sum);
-            }
-            //class average
-            for (i = 0; i < 4; i++)
-            {
-                sum = 0;
-                for (j = 0; j < 12; j++)
-                {
-                    sum = sum + Marks[j, i];
-                }
-                m[i] = sum / 12;
-                Console.WriteLine("Class average of subject {0} is: {1}", i, m[i]);
-            }
-            //standard deviation
-            for (i = 0; i < 4; i++)
-            {
-                sum = 0;
-                for (j = 0; j < 12; j++)
-                {
-                    sum = sum + (Marks[j, i] - m[i]) * (Marks[j, i] - m[i]);
+
                 }
 
-                Console.WriteLine("Standard deviation of subject {0} is: {1}", i, Math.Sqrt(sum / 12));
+                Console.WriteLine("student {0}, {1}", i + 1, sum);
             }
-            Console.WriteLine("Overall average is:{0}", Convert.ToDouble(m[0] + m[1] + m[2] + m[3]) / 4);
 
-            Console.ReadLine();
+            //b
+
+            for (int j = 0; j < Marks.GetLength(1); j++)
+            {
+                sum = 0;
+                for (int i = 0; i < Marks.GetLength(0); i++)
+                {
+                    sum = sum + Marks[i, j];
+
+                }
+
+                Console.WriteLine("subject {0}, {1}", j + 1, sum / 12);
+            }
+            //c
+
+            int sum1 = 0;
+            for (int i = 0; i < Marks.GetLength(0); i++)
+            {
+                //  sum = 0;
+                for (int j = 0; j < Marks.GetLength(1); j++)
+                {
+                    sum1 = sum1 + Marks[i, j];
+                }
+            }
+                Console.WriteLine("{0}", sum1/48);
+            }
         }
     }
-}
+
+
+    
+
+
+
     
         
